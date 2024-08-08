10cc9bcaaaeb2e10218675727439ae67a68b698e
Theo Lardeur Gersztein <lardeurgerszteintheo@gmail.com>
2024-08-08 11:43:36 +0200

    Hidden menu and AddProfile pop-up

--- frontend/src/AccountsProfiles/AddProfile.js
+++ frontend/src/AccountsProfiles/AddProfile.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from "react";
 import {addProfileAPI, getAccountIdAPI} from "../api";
 import {useNavigate} from "react-router-dom";
+import "./AddProfile.css";
 
-function AddProfile() {
+
+function AddProfile({isOpen, onClose}) {
 
     const [name, setName] = useState('');
     const navigate = useNavigate();
+    const [error, setError] = useState('');
+
 
     useEffect(() => {
         const fetchAccountId = async () => {
@@ -14,25 +18,42 @@ function AddProfile() {
                 navigate("/");
                 return;
             }
-
         };
         fetchAccountId();
-    }, );
-    const handleAddprofile = () => {
-        addProfileAPI(name)
+    }, [navigate]);
+    const handleAddProfile = async () => {
+        if (!name) {
+            setError('Name cannot be empty');
+            return;
+        }
+        await addProfileAPI(name)
+        onClose()
         navigate("/profiles")
     }
 
+    if (!isOpen) {
+        return null; // Don't render the modal if it's not open
+    }
+
     return (
-        <div>
-            <input
-                className="name-input"
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <button className="add-profile-button" onClick={handleAddprofile}>Add Profile</button>
+        <div className="modal-overlay">
+            <div className="modal">
+                <div className="modal-header">
+                    <h2>Add Profile</h2>
+                    <button className="close-button" onClick={onClose}>&times;</button>
+                </div>
+                <div className="modal-body">
+                    <input
+                        className="name-input"
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    {error && <p className="error-message">{error}</p>}
+                    <button className="add-profile-button" onClick={handleAddProfile}>Add Profile</button>
+                </div>
+            </div>
         </div>
     )
 }
