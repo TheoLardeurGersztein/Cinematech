3153821781fa56955d23ba3ba649c474f415d16a
TheoTSP <lardeurgerszteintheo@gmail.com>
2024-08-19 22:31:13 +0200

    Fixed Discover and Search (react and django)

---
+++ frontend/src/Loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Loading.css'
+
+function Loading() {
+
+
+    return (
+        <div className="loading-header">
+            <div className="loading-bar"></div>
+        </div>
+    )
+
+}
+
+export default Loading
